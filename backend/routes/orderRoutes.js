03013003d4450d10d09e305a55c23d62ad18fca1
shizhong <shizhong1989@gmail.com>
2023-04-02 20:11:46 -0700

    Add backend for orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,6 +4,7 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -12,6 +13,7 @@ import { protect } from "../middleware/authMiddleware.js";
 // @access Pulic
 
 router.route("/").post(protect, addOrderItems);
+router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
 
