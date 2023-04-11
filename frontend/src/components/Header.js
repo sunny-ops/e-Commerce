31bebc46687340d0c9e74690c17317a4bcf7a4f4
shizhong <shizhong1989@gmail.com>
2023-04-11 15:13:15 -0700

    Admin ProductList

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -47,7 +47,7 @@ const Header = () => {
                   <LinkContainer to="/admin/userlist">
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to="/dmin/productlist">
+                  <LinkContainer to="/admin/productlist">
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
                   <LinkContainer to="/admin/orderlist">
