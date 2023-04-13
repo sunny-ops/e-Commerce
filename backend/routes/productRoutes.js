2121b52cb053529ef92622affea00ccf305d6f47
shizhong <shizhong1989@gmail.com>
2023-04-12 20:53:39 -0700

    Create and update product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,17 +3,25 @@ const router = express.Router();
 import {
   getProductById,
   getProducts,
+  deleteProduct,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 // @desct Fetch all products
 // @route GET /api/products
 // @access Pulic
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
 // @desct Fetch sigle product
 // @route GET /api/products/:id
 // @access Pulic
-router.route("/:id").get(getProductById);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
