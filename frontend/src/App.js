d75ef39a0721005104f17ff5655748e7f00e191b
shizhong <shizhong1989@gmail.com>
2023-04-14 09:34:22 -0700

    Admin Order List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
+import OrderListScreen from "./screens/OrderListScreen";
 
 const App = () => {
   return (
@@ -41,6 +42,7 @@ const App = () => {
               path="/admin/product/:id/edit"
               element={<ProductEditScreen />}
             />
+            <Route path="/admin/orderlist" element={<OrderListScreen />} />
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
           </Routes>
         </Container>
