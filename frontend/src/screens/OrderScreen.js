a1cded1b793bfafe32a1f9310f2be6160255df44
shizhong <shizhong1989@gmail.com>
2023-04-20 11:56:55 -0700

    change to use react-paypal-js

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { PayPalButton } from "react-paypal-button-v2";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { Link } from "react-router-dom";
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -201,10 +202,17 @@ const OrderScreen = () => {
                   {!sdkReady ? (
                     <Loader />
                   ) : (
-                    <PayPalButton
-                      amount={order.totalPrice}
-                      onSuccess={successPaymenHandler}
-                    />
+                    // <PayPalButton
+                    //   amount={order.totalPrice}
+                    //   onSuccess={successPaymenHandler}
+                    // />
+                    <PayPalScriptProvider options={{ "client-id": "test" }}>
+                      <PayPalButtons
+                        style={{ layout: "horizontal" }}
+                        amount={order.totalPrice}
+                        onSuccess={successPaymenHandler}
+                      />
+                    </PayPalScriptProvider>
                   )}
                 </ListGroup.Item>
               )}
